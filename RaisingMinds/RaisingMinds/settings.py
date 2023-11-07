093daedd4fbef6f5bea1b2ed330439c2a4661a57
Dulitha Harasara <129366975+DulithaHarasara@users.noreply.github.com>
2023-11-07 10:44:24 +0530

    build custom user model

--- RaisingMinds/RaisingMinds/settings.py
+++ RaisingMinds/RaisingMinds/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# mention that what is the auth user model
+AUTH_USER_MODEL = 'users.User'
