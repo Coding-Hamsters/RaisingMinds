33cd90d38aca359135d76d9cf3625f25fd6e385c
Dulitha Harasara <129366975+DulithaHarasara@users.noreply.github.com>
2023-11-09 10:51:22 +0530

    login and signup

--- RaisingMinds/RaisingMinds/urls.py
+++ RaisingMinds/RaisingMinds/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('app.urls'))
+    path('',include('app.urls')),
+    path('signup/',user_views.signup,name='signup'),
+    path('login/',user_views.login,name='login')
 ]
--- RaisingMinds/users/views.py
+++ RaisingMinds/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def signup(request):
+
+    return render(request,'users/signup.html')
+
+def login(request):
+
+    return render(request,'users/login.html')
