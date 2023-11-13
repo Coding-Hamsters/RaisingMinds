5153952450f2ca6cfe8f69204f3bf41ee660f071
Dulitha Harasara <129366975+DulithaHarasara@users.noreply.github.com>
2023-11-13 15:14:52 +0530

    Navigation active status

--- RaisingMinds/app/views.py
+++ RaisingMinds/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from users.models import User
 from user_profile.models import Profile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -11,7 +12,7 @@ def index(request):
     except:
         profile = None
 
-    return render(request,'app/index.html',{'user':user,'profile':profile})
+    return render(request,'app/index.html',{'user':user,'profile':profile,'navbar':'index'})
 
 def home(request):
 
@@ -19,6 +20,7 @@ def home(request):
 
     return render(request,'app/home.html',{'user':user})
 
+
 def project(request):
 
-    return render(request,'app/projects.html')
+    return render(request,'app/projects.html',{'navbar':'projects'})
