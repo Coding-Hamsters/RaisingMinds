338ea8c04b3ba35083be2a8f74d9d5f9679eea7a
Dulitha Harasara <129366975+DulithaHarasara@users.noreply.github.com>
2023-11-13 13:49:54 +0530

    update user_profile model

--- RaisingMinds/app/views.py
+++ RaisingMinds/app/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from users.models import User
+from user_profile.models import Profile
 
 # Create your views here.
 def index(request):
 
     user = request.user
+    try:
+        profile = Profile.objects.get(user = request.user)
+    except:
+        profile = None
 
-    return render(request,'app/index.html',{'user':user})
+    return render(request,'app/index.html',{'user':user,'profile':profile})
 
 def home(request):
 
--- RaisingMinds/user_profile/models.py
+++ RaisingMinds/user_profile/models.py
@@ -3,7 +3,7 @@ from users.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
     forget_password_token = models.CharField(max_length=100,null = True,blank=True)
     profile_image = models.ImageField(upload_to='profile_images',default='profile_images/defalt_profile_image.jpg')
 
