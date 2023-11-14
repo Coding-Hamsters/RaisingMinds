8501d3192a24eff2e88a5855dc998b377b1bf8d4
Dulitha Harasara <129366975+DulithaHarasara@users.noreply.github.com>
2023-11-14 14:21:31 +0530

    schoolProfile model

--- RaisingMinds/school_profile/models.py
+++ RaisingMinds/school_profile/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class schoolProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    school_name = models.CharField(max_length=100)
+    principal_name = models.CharField(max_length=100)
+    province = models.CharField(max_length=100)
+    district = models.CharField(max_length=100)
+    address = models.TextField()
+    verification_doc = models.FileField(upload_to='schoolProfile_vetification_documents')
+    school_profile_img = models.ImageField(upload_to='schoolProfile_profile_images')
+    is_verified = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.school_name
+    
