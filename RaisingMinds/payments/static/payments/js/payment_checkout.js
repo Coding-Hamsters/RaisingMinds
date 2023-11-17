c1de184fb0357bdc010a2f863a1e3aad96751f76
Heshitha <hesithedg@gmail.com>
2023-11-17 11:34:49 +0530

    add payment_checkout.html,css,js files
    *add payment_checkout.html
    *add payment_checkout.css
    *add payment_checkout.js

---
+++ RaisingMinds/payments/static/payments/js/payment_checkout.js
@@ -0,0 +1,8 @@
+const donateAmountInput = document.querySelector('.donateAmount');
+const donateAmountLabel = document.querySelector('.js');
+const donateButton = document.getElementById('button-addon2');
+
+donateButton.addEventListener('click', () => {
+  const donateAmountValue = parseInt(donateAmountInput.value);
+  donateAmountLabel.textContent = `Donate amount : $ ${donateAmountValue}.00`;
+});
