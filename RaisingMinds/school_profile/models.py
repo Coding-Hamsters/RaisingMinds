509ea8f7462aea0c98b77eaca7f60742ef167480
Dulitha Harasara <129366975+DulithaHarasara@users.noreply.github.com>
2023-11-18 07:38:53 +0530

    school profile create form

--- RaisingMinds/school_profile/models.py
+++ RaisingMinds/school_profile/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from users.models import User
+from django.utils import timezone
 
 # Create your models here.
 # build the schoolProfile model
@@ -7,6 +8,7 @@ class schoolProfile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     school_name = models.CharField(max_length=100)
     school_type = models.CharField(max_length=20,blank=True)
+    Description = models.TextField(blank=True)
     principal_name = models.CharField(max_length=100)
     province = models.CharField(max_length=100)
     district = models.CharField(max_length=100)
@@ -15,6 +17,7 @@ class schoolProfile(models.Model):
     school_telephone = models.CharField(max_length=10,blank=True)
     verification_doc = models.FileField(upload_to='schoolProfile_vetification_documents')
     school_profile_img = models.ImageField(upload_to='schoolProfile_profile_images')
+    date_created = models.DateTimeField(default=timezone.now)
     is_verified = models.BooleanField(default=True)
 
     def __str__(self):
--- RaisingMinds/school_profile/views.py
+++ RaisingMinds/school_profile/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.http import HttpResponse
 from user_profile.models import Profile
 from .models import schoolProfile
 from post.models import Post
@@ -22,9 +23,17 @@ def schoolprofile(request,pk):
         school_profile = None
 
     try:
+        # filter posts creaed by current school profile
         posts = Post.objects.all().filter(author = school_profile)
     except:
         posts = None
+
+    context = {
+        'user':user,
+        'profile':profile,
+        'school_profile':school_profile,
+        'posts':posts
+    }
 
     if request.method == 'POST':
         title = request.POST.get('title')
@@ -32,14 +41,89 @@ def schoolprofile(request,pk):
         goal_donation = request.POST.get('goal_donation')
         post_img = request.FILES.get('post_img')
 
-        new_post = Post.objects.create(author = school_profile,title = title,content = description,donate_amount = goal_donation,post_image = post_img)
+        # pass the variable into post model
+        new_post = Post.objects.create(
+            author = school_profile,
+            title = title,
+            content = description,
+            donate_amount = goal_donation,
+            post_image = post_img)
+        
+        # save the new post
         new_post.save()
 
         
     
 
-    return render(request,'school_profile/school_profile.html',{'user':user,'profile':profile,'school_profile':school_profile,'posts':posts})
-
+    return render(request,'school_profile/school_profile.html',context)
+# method for create new school profile
 def createSchoolProfile(request):
+
+    user = request.user
+    try:
+        # get current authenticated user's profile
+        profile = Profile.objects.get(user = request.user)
+    except:
+        profile = None
+
+    # try:
+    #      # get current authenticated user's school profile pk
+    #     school_profile = schoolProfile.objects.get()
+    # except:
+    #     school_profile = None
+
+    try:
+        # filter posts creaed by current school profile
+        posts = Post.objects.all().filter(author = school_profile)
+    except:
+        posts = None
+
+    context = {
+        'user':user,
+        'profile':profile,
+        # 'school_profile':school_profile,
+        'posts':posts
+    }
     
-    return render(request,'school_profile/school_registration.html')
+
+    if request.method == 'POST':
+
+        if schoolProfile.objects.filter(user = user).exists():
+            return HttpResponse('<h1>You already have a school profile</h1>')
+
+        schoolName = request.POST.get('schoolName')
+        schoolPrincipalName = request.POST.get('principalName')
+        schoolEmail = request.POST.get('schoolEmail')
+        schoolProvince = request.POST.get('schoolProvince')
+        schoolDistrict = request.POST.get('schoolDistrict')
+        schoolTeleNo = request.POST.get('phoneNo')
+        schoolType = request.POST.get('schoolType')
+        schoolAddress = request.POST.get('schoolAddress')
+        schoolDescription = request.POST.get('description')
+        schoolVerificationDoc = request.FILES.get('verificationDoc')
+        schoolProfileImage = request.FILES.get('schoolProfileImage')
+
+        # create new school profile
+        new_school_profile = schoolProfile.objects.create(
+            user = user,
+            school_name = schoolName,
+            school_type = schoolType,
+            Description = schoolDescription,
+            principal_name = schoolPrincipalName,
+            province = schoolProvince,
+            district = schoolDistrict,
+            address = schoolAddress,
+            school_email = schoolEmail,
+            school_telephone = schoolTeleNo,
+            verification_doc = schoolVerificationDoc,
+            school_profile_img = schoolProfileImage,
+            # date_created = models.DateTimeField(default=timezone.now)
+            # is_verified = models.BooleanField(default=True)
+        )
+
+        # save new school profile
+        new_school_profile.save()
+
+        return redirect('index')
+
+    return render(request,'school_profile/school_registration.html',context)
