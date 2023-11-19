fc7040b14b45ecb1a1381d425f5aeedcdacfb9ff
Dulitha Harasara <129366975+DulithaHarasara@users.noreply.github.com>
2023-11-19 11:24:24 +0530

    Update admin.py

--- RaisingMinds/users/admin.py
+++ RaisingMinds/users/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 # Register your models here.
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email','username','date_joined','is_active','is_staff','is_superuser')
+    search_fields = ('email','username',)
+    readonly_fields = ('date_joined',)
+    list_filter = ('date_joined',)
+    fieldsets = ()
+
+    add_fieldsets = (
+        (None, {
+            'classes':('wide'),
+            'fields':('email','username','password1','password2'),
+        }),
+    )
+    
+
+
+    ordering = ('date_joined',)
+admin.site.register(User,CustomUserAdmin)
