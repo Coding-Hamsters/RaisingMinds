88d5f610031900762ce285cce072861d911e4412
Dulitha Harasara <129366975+DulithaHarasara@users.noreply.github.com>
2023-12-07 09:09:07 +0530

    404 error page

--- RaisingMinds/app/urls.py
+++ RaisingMinds/app/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('home/',views.home,name='home'),
-    path('projects/',views.project,name='projects')
+    path('projects/',views.project,name='projects'),
+    path('error404/',views.error404,name='error404')
 ]
 
 
